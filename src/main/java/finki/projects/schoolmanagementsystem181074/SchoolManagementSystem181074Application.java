f1a44229813d94f0ddae5d099642060e916c34a5
Aleksandra Jovevska <74111425+jovevskaaleksandra@users.noreply.github.com>
2022-04-22 18:20:38 +0200

    Initial commit

---
+++ src/main/java/finki/projects/schoolmanagementsystem181074/SchoolManagementSystem181074Application.java
@@ -0,0 +1,13 @@
+package finki.projects.schoolmanagementsystem181074;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementSystem181074Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementSystem181074Application.class, args);
+    }
+
+}
---
+++ src/test/java/finki/projects/schoolmanagementsystem181074/SchoolManagementSystem181074ApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.projects.schoolmanagementsystem181074;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystem181074ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
